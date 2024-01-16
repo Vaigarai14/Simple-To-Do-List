082ea212757f667cd8934a2f6869efddeb90412c
Vaigarai14 <vaigaraithennarasu144@gmail.com>
2024-01-17 02:23:45 +0530

    worked in local storage element

--- main.js
+++ main.js
@@ -2,33 +2,47 @@ const inputel = document.getElementById('inputel');
 const addbtn = document.getElementById('addbtn');
 const ul = document.getElementById('ul')
 
-console.log(addbtn);
+// //! create remove btn
+function span(li) {
+    let span = document.createElement('span')
+    span.innerText = "×"      //\u00d7
+    li.appendChild(span)
+}
 
+document.addEventListener('DOMContentLoaded', () => {
+    const fetchdata = [...JSON.parse(localStorage.getItem('data'))];
+    fetchdata.forEach((data) => {
+        let li = document.createElement('li')
+        let litxt = document.createTextNode(data.inputdata)
+        li.appendChild(litxt)
+        ul.appendChild(li)
+        span(li)
+    })
+
+})
+// & Create li element
 function createelement() {
     if (inputel.value.length > 3) {
         let li = document.createElement('li')
         let litxt = document.createTextNode(inputel.value)
         li.appendChild(litxt)
         ul.appendChild(li)
-        let span = document.createElement('span')
-        span.innerHTML = "\u00d7"
-        li.appendChild(span)
-        data()
+        span(li)
+        localStorage.setItem('data', JSON.stringify(
+            [...JSON.parse(localStorage.getItem('data')
+                || "[]"), { inputdata: inputel.value }
+            ])
+        );
     }
     else {
         alert("⚠ Enter any value and Enter more than three character")
-        data()
     }
+
     inputel.value = ''
 }
 
-inputel.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-        createelement()
-    }
-})
+//^ Icon action
 
-addbtn.addEventListener('click', createelement)
 
 ul.addEventListener('click', (e) => {
     if (e.target.localName === 'li') {
@@ -36,15 +50,29 @@ ul.addEventListener('click', (e) => {
     }
     else if (e.target.localName === 'span') {
         e.target.parentElement.remove();
+
+        const fetchdata = [...JSON.parse(localStorage.getItem('data'))];
+        fetchdata.forEach((item) => {
+            var a = item.inputdata + "×"
+            if (a === e.target.parentElement.innerText) {
+                fetchdata.splice(fetchdata.indexOf(item), 1)
+            }
+        })
+
+        localStorage.setItem('data', JSON.stringify(fetchdata))
     }
-}, false);
+});
 
-function data() {
-    localStorage.setItem('data', ul.innerHTML)
-}
+inputel.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        createelement();
+    }
+})
 
-function showtask() {
-    ul.innerHTML = localStorage.getItem('data');
-}
+addbtn.addEventListener('click', (e) => {
+    createelement();
+})
 
-showtask()
+// local storage
+
+
